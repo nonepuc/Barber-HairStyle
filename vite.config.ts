1db487888fa79dfc4936047576fd563f841e082d
fuckpuc <jokerhambardzumyan@gmail.com>
2025-06-07 19:33:26 +0300

    last try

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,24 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
+      }
+    },
+    sourcemap: true,
+    minify: 'terser'
   },
   server: {
     port: 3000,
-    open: true,
+    open: true
   }
 });
