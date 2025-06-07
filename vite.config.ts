3b7bde6630ae9ea551b4ed8663a337acd8ffa9b9
fuckpuc <jokerhambardzumyan@gmail.com>
2025-06-07 19:47:37 +0300

    last try 2

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 export default defineConfig({
+  base: '/Barber-HairStyle/', // Replace with your repository name
   plugins: [vue()],
   resolve: {
     alias: {
