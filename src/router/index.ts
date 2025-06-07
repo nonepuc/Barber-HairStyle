2fc1cb4bb83385434ada37019783853de0c4af09
fuckpuc <jokerhambardzumyan@gmail.com>
2025-06-07 20:57:19 +0300

    deploying

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import HomePage from './../pages/HomePage.vue';
 
 const routes = [
@@ -10,6 +10,6 @@ const routes = [
 ];
 
 export const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
